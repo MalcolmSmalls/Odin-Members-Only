c11b7dc63bd5739616ebcad9900b116b1d64dcd6
MalcolmSmalls <smallsbeats@gmail.com>
2022-12-29 11:07:49 -0800

    Add form for log-in page

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router()
 
 
 router.get("/", (req, res, next) => {
-    res.render("index", { title: "Log-In" });
+    res.render("log-in-form", { title: "Log-In" });
   });
 
 
